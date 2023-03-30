0c98d643445307ce6ddc52a49750260f102b95e1
Matheus F. Dias <57161520+Foscariny@users.noreply.github.com>
2023-03-29 21:37:12 -0300

    feat(about): adding about simple text

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,59 +1,34 @@
 <template>
-  About route
+  <div class="about-container">
+    <h1 class="title">About</h1>
+    <div class="text-container">
+      <p>Well, for now this is just a simple project to learn the changes in Vue 3. I'm using the Composition API and the Pinia, a state management library.</p>
+      <p>I'm really used to Vue 2, and soon we'll start migrating our projects to Vue 3, so I'm trying to learn as much as I can about the new features.</p>
+      <p>Soon I'll add more stuff to this project, and even start another one with TypeScript.</p>
+    </div>
+  </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 
-.home-container {
+.about-container {
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-}
-.title {
-  font-size: 2.5rem;
-  font-weight: 700;
-  color: #333;
-  margin-bottom: 1rem;
-}
+  & .title {
+    font-size: 2.5rem;
+    font-weight: 700;
+    color: #333;
+    margin-bottom: 1rem;
+  }
 
-.pot {
-  width: 400px;
-  height: fit-content;
-  align-self: center;
-  transition: all 0.3s ease;
-  animation: float 4s ease-in-out infinite;
-  margin-top: -300px;
-}
-
-.hearts {
-  width: 800px;
-  height: fit-content;
-  animation: vertical-floating 6s ease-in-out infinite;
-}
-
-@keyframes float {
-  0% {
-    transform: translateY(0);
-  }
-  50% {
-    transform: translateY(-10px);
-  }
-  100% {
-    transform: translateY(0);
+  & .text-container {
+    font-size: 1.2rem;
+    color: #333;
+    text-align: center;
+    line-height: 1.5;
+    max-width: 600px;
   }
 }
-
-@keyframes vertical-floating {
-  0% {
-    transform: translateY(0);
-  }
-  50% {
-    transform: translateY(-5px);
-  }
-  100% {
-    transform: translateY(0);
-  }
-}
-
 </style>
