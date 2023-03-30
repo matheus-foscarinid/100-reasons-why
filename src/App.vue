cee5b3a0eaf750f96a94d1664db746fc1c51ae22
Matheus F. Dias <57161520+Foscariny@users.noreply.github.com>
2023-03-29 21:27:35 -0300

    feat(About): adding about base route

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <nav class="header">
     <router-link to="/">Home</router-link>
     <router-link to="/about">About</router-link>
-    <router-link to="/contact">Contact</router-link>
   </nav>
 
   <RouterView />
