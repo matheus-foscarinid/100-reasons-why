f61f275f426724b227ca20b8de00eb8676638081
Matheus F. Dias <57161520+Foscariny@users.noreply.github.com>
2023-03-30 21:23:06 -0300

    feat(reasonsService): adding the reasons service to get the reasons. Fot it's using a simple JSON

---
+++ src/services/reasons.service.js
@@ -0,0 +1,43 @@
+import fs from 'fs';
+
+const JSON_PATH = 'src/db/100-reasons.json';
+
+const getAllReasons = async () => {
+  try {
+    const json = fs.readFileSync(JSON_PATH, 'utf8');
+
+    return JSON.parse(json);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+const getReasonByIndex = async (index) => {
+  try {
+    const reasonsJSON = await getAllReasons();
+
+    return reasonsJSON[index];
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+const pushReason = async (reason) => {
+  try {
+    const reasonsJSON = await getAllReasons();
+    
+    reasonsJSON.push(reason);
+    fs.writeFileSync(JSON_PATH, JSON.stringify(reasonsJSON));
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const reasonsService = {
+  getAllReasons,
+  getReasonByIndex,
+  pushReason
+};
