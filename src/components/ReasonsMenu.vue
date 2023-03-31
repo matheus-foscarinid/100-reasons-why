3fd32a507a7218517c3a147a5330a5c6e10763fb
Matheus F. Dias <57161520+Foscariny@users.noreply.github.com>
2023-03-31 20:19:08 -0300

    feat(ReasonsMenu): starting the reasons menu where the buttons will be in

---
+++ src/components/ReasonsMenu.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <input type="text">
+  </div>
+</template>
+
+<script setup>
+  
+</script>
+
+<style scoped>
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background: #333;
+    padding: 1rem;
+  }
+</style>
