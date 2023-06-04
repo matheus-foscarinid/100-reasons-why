9e27db450ee8e7cbd4bc5fcb77e03ac177a3897d
Matheus Dias <matheus.foscarinid@gmail.com>
2023-06-04 19:44:21 -0300

    WIP test(ReasonsService): adding some tests to reasons service.

--- src/services/reasons.service.spec.js
+++ src/services/reasons.service.spec.js
@@ -2,8 +2,17 @@ import { describe, test, expect } from 'vitest';
 import { reasonsService } from './reasons.service.js';
 
 describe('Reasons service', () => {
-  test('getAllReasons should return an array of reasons', async () => {
-    const reasons = await reasonsService.getAllReasons();
+  test('getAllReasons should return an array of reasons', () => {
+    const reasons = reasonsService.getAllReasons();
     expect(Array.isArray(reasons)).toBe(true);
+  });
+
+  test('getReasonByIndex should return a reason', () => {
+    const jsonMock = [{ id: 1, reason: 'Test reason' }];
+    reasonsService.getAllReasons = () => jsonMock;
+
+    const reason = reasonsService.getReasonByIndex(0);
+
+    expect(reason).toBe(jsonMock[0]);
   });
 });
